802e49ca10d016eb68a2b14b3c6f4509a5da96df
NguyenCD <dinhnguyen.k15@gmail.com>
2022-04-25 21:48:32 +0700

    [Implement] - Implement Footer component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import Story from "./components/Story";
 import Products from "./components/Products";
 import Testimonial from "./components/Testimonial";
 import NewArrivals from "./components/NewArrivals";
+import NewsLetter from "./components/NewsLetter";
+import Footer from "./components/Footer";
+import ScrollUp from "./components/ScrollUp";
 import "./css/base.css";
 import "./css/media.css";
+
 function App() {
   return (
     <div>
@@ -19,7 +23,10 @@ function App() {
         <Products></Products>
         <Testimonial></Testimonial>
         <NewArrivals></NewArrivals>
+        <NewsLetter></NewsLetter>
       </main>
+      <Footer></Footer>
+      <ScrollUp></ScrollUp>
     </div>
   );
 }
